5da581b5b70958028ca1536f67d55e1de02e733a
gasangw <thomasgasangwa@gmail.com>
2023-08-14 17:21:52 +0200

    Add color to the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-ojemba': '#8ACC50',
+      },
+    },
   },
   plugins: [],
 }
