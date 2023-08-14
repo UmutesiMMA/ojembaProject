ff07a665e13cef4a8189a14d9d97dc1d1e29ef3b
gasangw <thomasgasangwa@gmail.com>
2023-08-14 19:10:02 +0200

    Add container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['index.html','./js/index.js'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem'
+      }
+    },
     extend: {
       colors: {
         'green-ojemba': '#8ACC50',
