14ab03f103e99654387f8265c03d5056797a0206
gsngdavid <gsngdavid@gmail.com>
2023-08-15 14:30:03 +0200

    feat: Add Montserrat font

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,8 @@ module.exports = {
     },
     fontFamily: {
       'roboto': 'Roboto, sans-serif',
-      'raleway': 'raleway, Helvetica'
+      'raleway': 'raleway, Helvetica',
+      'montserrat': 'Montserrat, Helvetica'
     }
   },
   plugins: [],
